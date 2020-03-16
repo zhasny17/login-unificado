329aefdd381cdc815598b79d628413a92fc2c227
lucas luz feitosa <lucasluz@fortalsistemas.com.br>
2020-03-16 15:12:02 -0300

    modelos de refresh e access token

--- models.py
+++ models.py
@@ -35,3 +35,49 @@ class User(db.Model):
     updated_at = db.Column(db.DateTime(timezone=True))
     removed_at = db.Column(db.DateTime(timezone=True))
     removed = db.Column(db.Boolean, nullable=False, default=False)
+
+
+class AccessToken(db.Model):
+    __table_args__ = (
+        tables_config
+    )
+    __tablename__ = 'access_token'
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    valid = db.Column(db.Boolean, nullable=False, default=False)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", backref=db.backref("access_token", lazy=True))
+    refresh_token_id = db.Column(db.String(36), db.ForeignKey('refresh_token.id'), nullable=False)
+    refresh_token = db.relationship("RefreshToken", backref=db.backref("access_token", lazy=True))
+
+    def has_expired(self, when=None):
+        if datetime.utcnow() >= self.expiration_date:
+            return True
+        else:
+            return False
+
+    def is_active(self):
+        return self.valid and not self.has_expired()
+
+
+class RefreshTokens(db.Model):
+    __table_args__ = (
+        tables_config
+    )
+    __tablename__ = 'refresh_token'
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    valid = db.Column(db.Boolean, nullable=False, default=False)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", backref=db.backref("refresh_token", lazy=True))
+
+    def has_expired(self):
+        if datetime.utcnow() >= self.expiration_date:
+            return True
+        else:
+            return False
+
+    def is_active(self):
+        return self.valid and not self.has_expired()
